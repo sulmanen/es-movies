47b4328a9368ee0290c3208a0239c7d860a5927d
Samuli Ulmanen <samuli.ulmanen@wunderdog.fi>
2019-03-28 10:36:31 +0200

    nth and format

--- relevance/test/relevance/search_test.clj
+++ relevance/test/relevance/search_test.clj
@@ -3,17 +3,23 @@
             [relevance.core :refer :all]
             [relevance.search :refer [search!]]))
 
+(defn get-nth [res field n]
+  (get-in (nth (get-in res ["hits" "hits"]) n) ["_source" field]))
+
 (defn get-first-hit [res field]
-  (get-in (first (get-in res ["hits" "hits"])) ["_source" field]))
+  (get-nth res field 0))
 
 (deftest can-query
   (testing "We can query for woman and get results"
-    (is (= (get-first-hit (search! "woman" 0 25) "title") "Woman to Woman"))))
+    (is (= (get-first-hit (search! "woman" 0 25) "title")
+           "Woman to Woman"))))
 
 (deftest can-query-by-director
   (testing "Can query by director"
-    (is (= (get-first-hit (search! "tarantino" 0 25) "director") "Tarantino"))))
+    (is (= (get-first-hit (search! "tarantino" 0 25) "director")
+           "Tarantino"))))
 
 (deftest query-by-year
   (testing "query by year"
-    (is (= (get-first-hit (search! "1977" 0 25) "year") "1977"))))
+    (is (= (get-first-hit (search! "1977" 0 25) "year")
+           "1977"))))

fd262ce13338bb4c0fea8450e8daf1d7db4f04c6
Samuli Ulmanen <samuli.ulmanen@wunderdog.fi>
2019-03-29 15:27:43 +0200

    some changes while presenting

--- relevance/test/relevance/search_test.clj
+++ relevance/test/relevance/search_test.clj
@@ -15,12 +15,15 @@
 (defn ordered [hits]
   (if (reduce (fn [first second]
                 (if (and first (>= first second)) second false))
-              (map (fn [hit] (read-string (subs (get-in hit ["_source" "year"]) 0 4))) hits)) true false))
+              (map (fn [hit] (read-string (subs (get-in hit ["_source" "year"]) 0 4))) hits))
+    true
+    false))
 
 (defn pluck [hits field]
   (map (fn [hit] (get-in hit ["_source" field])) hits))
 
-(deftest can-query
+
+(deftest can-query-title
   (testing "We can query for pulp and get results"
     (is (= (pluck (get-hits (search! "pulp" 0 6)) "title")
            ["Pulp Fiction"]))))
@@ -40,13 +43,28 @@
     (is (= (pluck (get-hits (search! "sound" 0 6)) "title")
            ["The Sound of Music"
             "The Sound and the Fury"
-            "The Sound Barrier"]))))
+            "The Sound Barrier"
+]))))
 
-(deftest query-by-year
-  (testing "query by year"
-    (is (= (get-first-hit (search! "1977" 0 6) "year")
-           "1977-01-01T00:00:00"))))
+(deftest can-query-by-year
+  (testing "Can query by title"
+    (is (= (pluck (get-hits (search! "1977" 0 6)) "title")
+           ["L'Diable probalement"
+            "The Spy Who Loved Me"
+            "Man of Marble" "Matababi"
+            "The Island of Dr.~Moreau"
+            "Carry On Emanuelle"]))))
+
+(deftest can-query-by-partial-year
+  (testing "Can query by title"
+    (is (= (pluck (get-hits (search! "197" 0 6)) "title")
+           ["Dreamer"
+            "The Innocent"
+            "Mysterious Island of Beautiful Women"
+            "All That Jazz"
+            "The Young Girls of Wilko"
+            "10"]))))
 
 (deftest ordered-by-year
   (testing "results ordered by year latest first"
-    (is (= (ordered (get-hits (search! "woman" 0 6))) true))))
+    (is (ordered (get-hits (search! "woman" 0 6))) true)))
